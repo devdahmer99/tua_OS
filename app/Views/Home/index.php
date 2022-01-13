d3918e3ddbedeec267d7e0fe7ac0d3278a8a3fe9
Eduardo Dahmer <eduardodahmer99@gmail.com>
2022-01-13 20:23:35 -0300

    Parametrizacao do projeto

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,1 +1,19 @@
-<?php
+<?php $this->extend('Layout/principal')?>
+
+<?php $this->section('titulo');?>
+    <?php echo $titulo;?>
+<?php $this->endsection();?>
+
+<?php $this->section('estilos');?>
+
+<?php $this->endsection();?>
+
+
+<?php $this->section('conteudo');?>
+
+<?php $this->endsection();?>
+
+
+<?php $this->section('scripts');?>
+
+<?php $this->endsection();?>
